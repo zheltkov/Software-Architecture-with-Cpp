abac703a6c1b4e56a5688bb06b4d9b23b10782f8
Alexandr Zheltkov <zheltkov@gmail.com>
2023-02-12 17:15:03 +0100

    format

--- Chapter06/05_niebloids.cpp
+++ Chapter06/05_niebloids.cpp
@@ -20,11 +20,10 @@ struct contains_fn final {
 
   template <std::ranges::input_range Range, typename T,
             typename Proj = std::identity>
-  requires std::indirect_binary_predicate<
-      std::ranges::equal_to,
+  requires std::indirect_binary_predicate < std::ranges::equal_to,
       std::projected<std::ranges::iterator_t<Range>, Proj>,
-      const T *> constexpr bool
-  operator()(Range &&range, const T &value, Proj projection = {}) const {
+      const T *> constexpr bool operator()(Range &&range, const T &value,
+                                           Proj projection = {}) const {
     return (*this)(std::ranges::begin(range), std::ranges::end(range), value,
                    std::move(projection));
   }
